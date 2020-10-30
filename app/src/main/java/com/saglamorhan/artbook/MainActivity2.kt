3724b63ebcfe24c46850c6bce084f4edb2847832
saglamorhan61 <saglamorhan.61@gmail.com>
2020-10-30 17:28:24 +0300

    Manifest dosyasına izin istenecek alan eklendi. Izin dangerous oldugu icin kullanici izin isteme eklendi. Izın verilmemisse ne yapilacagi eklendi.

--- app/src/main/java/com/saglamorhan/artbook/MainActivity2.kt
+++ app/src/main/java/com/saglamorhan/artbook/MainActivity2.kt
@@ -1,8 +1,12 @@
 package com.saglamorhan.artbook
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,15 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     fun selectImage(view : View){
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            //İzin verilmemisse calisir
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+        }else{
+            // İzin verilmisse calisir
+
+        }
 
 
     }
