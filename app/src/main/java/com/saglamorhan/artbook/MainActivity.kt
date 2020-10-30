90253e2563a91b426179ada1bd98d32cd3505a6d
saglamorhan61 <saglamorhan.61@gmail.com>
2020-10-30 17:16:47 +0300

    Menu Tanımlandı. Intent Olusturuldu

--- app/src/main/java/com/saglamorhan/artbook/MainActivity.kt
+++ app/src/main/java/com/saglamorhan/artbook/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.saglamorhan.artbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //Inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_art,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        if (item.itemId == R.id.add_art_item){
+
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
