be923c155920057c13825ad73862ddb4e0b923bd
saglamorhan61 <saglamorhan.61@gmail.com>
2020-10-30 17:49:22 +0300

    Galerideyken neler yapilacagi eklendi

--- app/src/main/java/com/saglamorhan/artbook/MainActivity2.kt
+++ app/src/main/java/com/saglamorhan/artbook/MainActivity2.kt
@@ -3,14 +3,20 @@ package com.saglamorhan.artbook
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
+
+    var selectedPicture :  Uri?  = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -52,7 +58,33 @@ class MainActivity2 : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //Galerideyken yapılanlar burada
 
+        if (requestCode== 2 && resultCode == RESULT_OK && data != null){
+
+            selectedPicture = data.data
+
+            if (selectedPicture != null){
+
+                if (Build.VERSION.SDK_INT >= 28){
+
+                    val source = ImageDecoder.createSource(this.contentResolver,selectedPicture!!)
+                    val bitmap = ImageDecoder.decodeBitmap(source)
+                    imageView.setImageBitmap(bitmap)
+
+                }else{
+
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,selectedPicture)
+                    imageView.setImageBitmap(bitmap)
+
+                }
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
     fun save(view : View){
 
