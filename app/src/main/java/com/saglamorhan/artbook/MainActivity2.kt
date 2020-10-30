8b13c062f6369412fecd58c29069b6d7e40d3000
saglamorhan61 <saglamorhan.61@gmail.com>
2020-10-30 16:55:28 +0300

    Arayuz Olusturuldu setOnClickler tanımlandı

---
+++ app/src/main/java/com/saglamorhan/artbook/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.saglamorhan.artbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+    }
+
+    fun selectImage(view : View){
+
+
+    }
+
+    fun save(view : View){
+
+    }
+}
