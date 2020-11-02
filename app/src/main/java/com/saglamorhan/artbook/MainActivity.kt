d14daa790f1efc99033953abffbd71fcb7101ce2
saglamorhan61 <saglamorhan.61@gmail.com>
2020-11-02 19:44:26 +0300

    -Veriler icin iki adet arraylist olusturuldu. -Bunlardan biri listview icerisine koymak digeri ise tıklanildiginda diger activiteye id sini gondermek ve orda kullanmak icin -Array adapter olusturuldu. -Database acilip gerekli veriler cekilip listelere konuldu. -listview degisiklerden haberdar edildi.

--- app/src/main/java/com/saglamorhan/artbook/MainActivity.kt
+++ app/src/main/java/com/saglamorhan/artbook/MainActivity.kt
@@ -5,11 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val artNameList = ArrayList<String>()
+        val artIdList = ArrayList<Int>()
+
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,artNameList)
+        listView.adapter = arrayAdapter
+
+        try {
+
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+
+            val cursor = database.rawQuery("SELECT * FROM arts",null)
+            val artNameIx = cursor.getColumnIndex("artname")
+            val idIx =  cursor.getColumnIndex("id")
+
+            while (cursor.moveToNext()){
+                artNameList.add(cursor.getString(artNameIx))
+                artIdList.add(cursor.getInt(idIx))
+
+            }
+            //listView i guncelle
+            arrayAdapter.notifyDataSetChanged()
+            cursor.close()
+
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
